e3b599c8f5fc08aeabe1095962858920a96fc7f4
Karan Khurana <kkhuran@L-SNVW3D5FD5-M.local>
2016-05-19 19:15:48 -0700

    First Commit

---
+++ Lab 1/FeedViewController.swift
@@ -0,0 +1,58 @@
+//
+//  FeedViewController.swift
+//  Lab 1
+//
+//  Created by Karan Khurana on 5/19/16.
+//  Copyright © 2016 Karan Khurana. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 1000)
+        scrollView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        // This method is called as the user scrolls
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView,
+                                  willDecelerate decelerate: Bool) {
+        // This method is called right as the user lifts their finger
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        // This method is called when the scrollview finally stops scrolling.
+    }
+
+    @IBOutlet weak var thrillistButton: UIButton!
+    @IBAction func thrillistOnClick(sender: AnyObject) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lab 1/NotificationsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedViewController.swift
+//  Lab 1
+//
+//  Created by Karan Khurana on 5/19/16.
+//  Copyright © 2016 Karan Khurana. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
